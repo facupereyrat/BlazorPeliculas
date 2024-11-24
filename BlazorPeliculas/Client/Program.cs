98d35481889823936cfec7409ebe2a602cd92894
Facu Pereyra <facupereyrat@outlook.com>
2024-11-23 23:12:22 -0300

    Correccion de rutas builder

--- BlazorPeliculas/Client/Program.cs
+++ BlazorPeliculas/Client/Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddSingleton<ServiciosSingleton>();
-builder.Services.AddTransient<ServiciosSingleton>();
+builder.Services.AddTransient<ServiciosTransient>();
 
 await builder.Build().RunAsync();
 
