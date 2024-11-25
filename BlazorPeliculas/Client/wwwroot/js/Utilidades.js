1e3d3e6bcee152802f87f1477212b71b75b55f10
Facu Pereyra <facupereyrat@outlook.com>
2024-11-24 22:43:30 -0300

    Invocar C# con javascript

---
+++ BlazorPeliculas/Client/wwwroot/js/Utilidades.js
@@ -0,0 +1,6 @@
+﻿function pruebnaPuntoNetStatic() {
+    DotNet.invokeMethodAsync("BlazorPeliculas.Client", "ObtenerCurrentCount") //invoco metodo 
+        .then(resultado => { //Luego de invocar entonces haces lo siguiente
+            console.log('conteo desde javascript' + resultados);
+        })
+}
